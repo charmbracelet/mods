ccf7e5611bf0114a2f19b9dfbe5c695ec644a9e8
Toby Padilla <toby@charm.sh>
2023-03-31 17:02:08 -0500

    chore: refactored because gpt-4 told me to

--- main.go
+++ main.go
@@ -14,32 +14,38 @@ import (
 	openai "github.com/sashabaranov/go-openai"
 )
 
-func usage() {
+func printUsage() {
 	fmt.Printf("Usage: %s [OPTIONS] [PREFIX TERM]\n", os.Args[0])
 	flag.PrintDefaults()
 }
 
-func main() {
-	modelVersionFlag := flag.String("m", "gpt-4", "OpenAI model flag.")
-	formatFlag := flag.Bool("f", false, "Ask GPT to format the output as Markdown.")
-	flag.Usage = usage
-	flag.Parse()
-
-	token := os.Getenv("OPENAI_API_KEY")
-	if token == "" {
-		log.Fatal("Error: OPENAI_API_KEY environment variable is required")
-	}
-
-	content := ""
+func readStdinContent() string {
 	if !isatty.IsTerminal(os.Stdin.Fd()) {
 		reader := bufio.NewReader(os.Stdin)
 		stdinBytes, err := ioutil.ReadAll(reader)
 		if err != nil {
 			log.Fatal("Error reading standard input: ", err)
 		}
-		content = string(stdinBytes)
+		return string(stdinBytes)
 	}
+	return ""
+}
 
+func createClient(apiKey string) *openai.Client {
+	if apiKey == "" {
+		log.Fatal("Error: OPENAI_API_KEY environment variable is required")
+	}
+	return openai.NewClient(apiKey)
+}
+
+func main() {
+	modelVersionFlag := flag.String("m", "gpt-4", "OpenAI model flag.")
+	formatFlag := flag.Bool("f", false, "Ask GPT to format the output as Markdown.")
+	flag.Usage = printUsage
+	flag.Parse()
+
+	client := createClient(os.Getenv("OPENAI_API_KEY"))
+	content := readStdinContent()
 	prefix := strings.Join(flag.Args(), " ")
 	if *formatFlag {
 		prefix = fmt.Sprintf("%s Format output as Markdown.", prefix)
@@ -49,7 +55,6 @@ func main() {
 		content = strings.TrimSpace(prefix + "\n\n" + content)
 	}
 
-	client := openai.NewClient(token)
 	resp, err := client.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
