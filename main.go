e904016380247ea1a09cd526efa7b7fc9d38b211
Toby Padilla <toby@charm.sh>
2023-03-31 19:00:46 -0500

    feat: -o flag to specify output file

--- main.go
+++ main.go
@@ -31,6 +31,21 @@ func readStdinContent() string {
 	return ""
 }
 
+func writeOutput(output, fileName string) {
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal("Error creating output file: ", err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	_, err = writer.WriteString(output)
+	if err != nil {
+		log.Fatal("Error writing to output file: ", err)
+	}
+	writer.Flush()
+}
+
 func createClient(apiKey string) *openai.Client {
 	if apiKey == "" {
 		log.Fatal("Error: OPENAI_API_KEY environment variable is required")
@@ -41,12 +56,17 @@ func createClient(apiKey string) *openai.Client {
 func main() {
 	modelVersionFlag := flag.String("m", "gpt-4", "OpenAI model flag.")
 	formatFlag := flag.Bool("f", false, "Ask GPT to format the output as Markdown.")
+	outputFileFlag := flag.String("o", "", "Output file to save response. If not specified, prints to console.")
 	flag.Usage = printUsage
 	flag.Parse()
 
 	client := createClient(os.Getenv("OPENAI_API_KEY"))
 	content := readStdinContent()
 	prefix := strings.Join(flag.Args(), " ")
+	if prefix == "" && content == "" {
+		printUsage()
+		os.Exit(0)
+	}
 	if *formatFlag {
 		prefix = fmt.Sprintf("%s Format output as Markdown.", prefix)
 	}
@@ -72,5 +92,10 @@ func main() {
 		log.Fatal("ChatCompletion error: ", err)
 	}
 
-	fmt.Println(resp.Choices[0].Message.Content)
+	gptContent := resp.Choices[0].Message.Content
+	if *outputFileFlag != "" {
+		writeOutput(gptContent, *outputFileFlag)
+	} else {
+		fmt.Println(gptContent)
+	}
 }
