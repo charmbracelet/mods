9dc6885bcce78616fdaa3a508922244cc4eaf7e9
Carlos A Becker <caarlos0@users.noreply.github.com>
2023-04-11 20:24:51 +0000

    fix: some linting issues
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- spinner.go
+++ spinner.go
@@ -11,15 +11,18 @@ type quitMsg struct{}
 var spinnerStyle = lipgloss.NewStyle().
 	Foreground(lipgloss.Color("212"))
 
+// Model for loading the results.
 type Model struct {
 	spinner  spinner.Model
 	quitting bool
 }
 
+// Init implements tea.Model.
 func (m Model) Init() tea.Cmd {
-	return spinner.Tick
+	return m.spinner.Tick
 }
 
+// Update implements tea.Model.
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case quitMsg:
@@ -37,6 +40,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, cmd
 }
 
+// View implements tea.Model.
 func (m Model) View() string {
 	if m.quitting {
 		return ""
