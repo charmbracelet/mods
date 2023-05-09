e61a33a2364bea26da09351cce6b5c3e7b03c266
Christian Rocha <christian@rocha.is>
2023-05-08 21:35:32 -0400

    feat: configure via the environment
    Co-authored-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- main.go
+++ main.go
@@ -103,7 +103,11 @@ func (nr noopRead) Read(_ []byte) (n int, err error) {
 func main() {
 	flag.Usage = usage
 	flag.CommandLine.SortFlags = false
-	config := newConfig()
+	config, err := newConfig()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	if config.Version {
 		fmt.Println(buildVersion())
 		os.Exit(0)
