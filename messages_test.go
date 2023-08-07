23697905526a59d70926c7669dcdd39b4932cce2
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-08-07 18:35:30 +0000

    test: add messages_test.go

---
+++ messages_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/sashabaranov/go-openai"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLastPrompt(t *testing.T) {
+	t.Run("no prompt", func(t *testing.T) {
+		require.Equal(t, "", lastPrompt(nil))
+	})
+
+	t.Run("single prompt", func(t *testing.T) {
+		require.Equal(t, "single", lastPrompt([]openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: "single",
+			},
+		}))
+	})
+
+	t.Run("multiple prompts", func(t *testing.T) {
+		require.Equal(t, "last", lastPrompt([]openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: "first",
+			},
+			{
+				Role:    openai.ChatMessageRoleSystem,
+				Content: "hallo",
+			},
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: "middle 1",
+			},
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: "middle 2",
+			},
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: "last",
+			},
+		}))
+	})
+}
+
+func TestFirstLine(t *testing.T) {
+	t.Run("single line", func(t *testing.T) {
+		require.Equal(t, "line", firstLine("line"))
+	})
+	t.Run("single line ending with \n", func(t *testing.T) {
+		require.Equal(t, "line", firstLine("line\n"))
+	})
+	t.Run("multiple lines", func(t *testing.T) {
+		require.Equal(t, "line", firstLine("line\nsomething else\nline3\nfoo\nends with a double \n\n"))
+	})
+}
