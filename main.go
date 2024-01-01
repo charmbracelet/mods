c115c8d012be8d690bf4298fcd62a12786f81814
Jessica Tarra <jessica@tarralva.com>
2024-01-01 17:14:50 -0300

    fix show cmd usage logic when match conditions

--- main.go
+++ main.go
@@ -127,7 +127,8 @@ var (
 				return resetSettings()
 			}
 
-			if config.ShowHelp || (config.Prefix == "" &&
+			if config.ShowHelp || (mods.Input == "" &&
+				config.Prefix == "" &&
 				config.Show == "" &&
 				!config.ShowLast &&
 				config.Delete == "" &&
