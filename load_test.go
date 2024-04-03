7ff723efac6ffa9a8c774a26f8e29a786834cb8a
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-04-03 15:33:46 -0300

    feat: allow to use role from URL/filepath
    This allows to set a role message to either a http/https URL or a
    filepath (prefixed with `file://`).
    
    Solves #235
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

---
+++ load_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoad(t *testing.T) {
+	const content = "just text"
+	t.Run("normal msg", func(t *testing.T) {
+		msg, err := loadMsg(content)
+		require.NoError(t, err)
+		require.Equal(t, content, msg)
+	})
+
+	t.Run("file", func(t *testing.T) {
+		path := filepath.Join(t.TempDir(), "foo.txt")
+		require.NoError(t, os.WriteFile(path, []byte(content), 0644))
+
+		msg, err := loadMsg("file://" + path)
+		require.NoError(t, err)
+		require.Equal(t, content, msg)
+	})
+
+	t.Run("http url", func(t *testing.T) {
+		msg, err := loadMsg("http://raw.githubusercontent.com/charmbracelet/mods/main/LICENSE")
+		require.NoError(t, err)
+		require.Contains(t, msg, "MIT License")
+	})
+
+	t.Run("https url", func(t *testing.T) {
+		msg, err := loadMsg("https://raw.githubusercontent.com/charmbracelet/mods/main/LICENSE")
+		require.NoError(t, err)
+		require.Contains(t, msg, "MIT License")
+	})
+}
