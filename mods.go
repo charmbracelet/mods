charmbracelet/mods

commit ef910e78f80b3de4641356623a4ac17397465626

fix: handle non-existent role (#244)
* fix: handle non-existent role

closes #239

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

* chore: fix msg

---------

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

mods.go
@@ -366,7 +366,14 @@ func (m *Mods) startCompletionCmd(content string) tea.Cmd {
 			})
 		}
 
-		for _, msg := range cfg.Roles[cfg.Role] {
+		roleSetup, ok := cfg.Roles[cfg.Role]
+		if !ok {
+			return modsError{
+				err:    fmt.Errorf("role %q does not exist", cfg.Role),
+				reason: "Could not use role",
+			}
+		}
+		for _, msg := range roleSetup {
 			content, err := loadMsg(msg)
 			if err != nil {
 				return modsError{

commit 7e0a4cc798eb479018d01cb099a9157df30c6287

fix: treat config key "role" as optional
Fixes: ef910e7 ("fix: handle non-existent role (#244)")
Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

mods.go
@@ -370,25 +370,27 @@ func (m *Mods) startCompletionCmd(content string) tea.Cmd {
 			})
 		}
 
-		roleSetup, ok := cfg.Roles[cfg.Role]
-		if !ok {
-			return modsError{
-				err:    fmt.Errorf("role %q does not exist", cfg.Role),
-				reason: "Could not use role",
-			}
-		}
-		for _, msg := range roleSetup {
-			content, err := loadMsg(msg)
-			if err != nil {
+		if cfg.Role != "" {
+			roleSetup, ok := cfg.Roles[cfg.Role]
+			if !ok {
 				return modsError{
-					err:    err,
+					err:    fmt.Errorf("role %q does not exist", cfg.Role),
 					reason: "Could not use role",
 				}
 			}
-			m.messages = append(m.messages, openai.ChatCompletionMessage{
-				Role:    openai.ChatMessageRoleSystem,
-				Content: content,
-			})
+			for _, msg := range roleSetup {
+				content, err := loadMsg(msg)
+				if err != nil {
+					return modsError{
+						err:    err,
+						reason: "Could not use role",
+					}
+				}
+				m.messages = append(m.messages, openai.ChatCompletionMessage{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: content,
+				})
+			}
 		}
 
 		if prefix := cfg.Prefix; prefix != "" {