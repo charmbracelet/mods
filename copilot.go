544f548778110299962f0b241133a891937fcd56
nathabonfim59 <dev@nathabonfim59.com>
2024-12-15 00:18:02 -0300

    feat: copilot AuthToken for Windows

--- copilot.go
+++ copilot.go
@@ -3,11 +3,21 @@ package main
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
+	"runtime"
 )
 
 func getCopilotAuthToken() (string, error) {
-	// TODO: Windows?
-	bts, err := os.ReadFile(os.Getenv("HOME") + "/.config/github-copilot/hosts.json")
+	var sessionPath string
+	if runtime.GOOS == "windows" {
+		// C:\Users\user\AppData\Local\github-copilot\hosts.json
+		sessionPath = filepath.Join(os.Getenv("LOCALAPPDATA"), "github-copilot", "hosts.json")
+	} else {
+		// ~/.config/github-copilot/hosts.json
+		sessionPath = filepath.Join(os.Getenv("HOME"), ".config/github-copilot", "hosts.json")
+	}
+
+	bts, err := os.ReadFile(sessionPath)
 	if err != nil {
 		return "", err
 	}
