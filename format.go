deca2cab8d4cf213ea8d45f7eac067272138febf
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-04-25 15:32:30 -0300

    fix: cache

--- format.go
+++ format.go
@@ -8,15 +8,49 @@ import (
 	"github.com/openai/openai-go"
 )
 
+type modsMessage struct {
+	Role       string                `json:"role"`
+	Content    string                `json:"content,omitempty"`
+	ToolCallID string                `json:"tool_call_id,omitempty"`
+	ToolCalls  []modsMessageToolCall `json:"tool_calls,omitempty"`
+}
+
+type modsMessageToolCall struct {
+	ID string `json:"id,omitempty"`
+}
+
+func convert(in *[]openai.ChatCompletionMessage) *[]modsMessage {
+	if in == nil {
+		return nil
+	}
+	var out []modsMessage
+	for _, msg := range *in {
+		mmsg := modsMessage{
+			Role:    string(msg.Role),
+			Content: msg.Content,
+		}
+		if tool := msg.ToParam().OfTool; tool != nil {
+			mmsg.ToolCallID = tool.ToolCallID
+		}
+		for _, call := range msg.ToolCalls {
+			mmsg.ToolCalls = append(mmsg.ToolCalls, modsMessageToolCall{
+				ID: call.ID,
+			})
+		}
+		out = append(out, mmsg)
+	}
+	return &out
+}
+
 func encode(w io.Writer, messages *[]openai.ChatCompletionMessage) error {
-	if err := gob.NewEncoder(w).Encode(messages); err != nil {
+	if err := gob.NewEncoder(w).Encode(convert(messages)); err != nil {
 		return fmt.Errorf("encode: %w", err)
 	}
 	return nil
 }
 
 func decode(r io.Reader, messages *[]openai.ChatCompletionMessage) error {
-	if err := gob.NewDecoder(r).Decode(messages); err != nil {
+	if err := gob.NewDecoder(r).Decode(convert(messages)); err != nil {
 		return fmt.Errorf("decode: %w", err)
 	}
 	return nil
