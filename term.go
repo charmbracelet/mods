32b0a4be6931c559595eaeaf4df8a0c913e1c7ce
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-06-05 17:03:47 -0300

    fix: use sync.OnceValue
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- term.go
+++ term.go
@@ -2,32 +2,33 @@ package main
 
 import (
 	"os"
+	"sync"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/mattn/go-isatty"
 	"github.com/muesli/termenv"
 )
 
-var isInputTTY = OnceValue(func() bool {
+var isInputTTY = sync.OnceValue(func() bool {
 	return isatty.IsTerminal(os.Stdin.Fd())
 })
 
-var isOutputTTY = OnceValue(func() bool {
+var isOutputTTY = sync.OnceValue(func() bool {
 	return isatty.IsTerminal(os.Stdout.Fd())
 })
 
-var stdoutRenderer = OnceValue(func() *lipgloss.Renderer {
+var stdoutRenderer = sync.OnceValue(func() *lipgloss.Renderer {
 	return lipgloss.DefaultRenderer()
 })
 
-var stdoutStyles = OnceValue(func() styles {
+var stdoutStyles = sync.OnceValue(func() styles {
 	return makeStyles(stdoutRenderer())
 })
 
-var stderrRenderer = OnceValue(func() *lipgloss.Renderer {
+var stderrRenderer = sync.OnceValue(func() *lipgloss.Renderer {
 	return lipgloss.NewRenderer(os.Stderr, termenv.WithColorCache(true))
 })
 
-var stderrStyles = OnceValue(func() styles {
+var stderrStyles = sync.OnceValue(func() styles {
 	return makeStyles(stderrRenderer())
 })
